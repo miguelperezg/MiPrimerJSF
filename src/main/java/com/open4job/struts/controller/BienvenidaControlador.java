dc8404bae9ed549a3a4c2b4049f6def75f9b84a3
miguelperezg <miguelperezg1986@gmail.com>
2015-02-05 14:01:48 +0100

    actionErrors añadidos cuando valor es vacío

--- src/main/java/com/open4job/struts/controller/BienvenidaControlador.java
+++ src/main/java/com/open4job/struts/controller/BienvenidaControlador.java
@@ -1,24 +1,48 @@
 package com.open4job.struts.controller;
 
+import org.apache.commons.validator.GenericValidator;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMessage;
 
 public class BienvenidaControlador extends ActionForm {
-	
+
 	String texto;
 	String nombre;
-	
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
+	public ActionErrors validate(
+			org.apache.struts.action.ActionMapping mapping,
+			javax.servlet.http.HttpServletRequest request) {
+		
+		ActionErrors errors = new ActionErrors();
+		
+		if (GenericValidator.isBlankOrNull(this.texto)) {
+			errors.add("operando1",
+					new ActionMessage("error.operando.negativo"));
+		}
+
+		if (GenericValidator.isBlankOrNull(this.nombre)) {
+			errors.add("operando2",
+					new ActionMessage("error.operando.negativo"));
+		}
+
+		return errors;
+	}
 
 }
--- src/main/java/com/open4job/struts/controller/Controlador.java
+++ src/main/java/com/open4job/struts/controller/Controlador.java
@@ -1,35 +1,35 @@
 package com.open4job.struts.controller;
 
-
-	import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-	 
+
 import javax.servlet.http.HttpSession;
 
-	import org.apache.struts.action.Action;
+import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-	 
-	//import com.mkyong.common.form.HelloWorldForm;
-	 
-	public class Controlador extends Action{
-	 
-		public ActionForward execute(ActionMapping mapping,ActionForm form,
-			HttpServletRequest request,HttpServletResponse response)
-	        throws Exception {
-	 /*
-			BienvenidaForm bienvenida = (BienvenidaForm) form;
-			bienvenida.setMessage("Bienvenido a Struts");
-	 */
-			
-			/*String texto = (String) request.getParameter("Texto");
-			String nombre = (String) request.getParameter("Nombre");
-			request.setAttribute("texto", texto);
-			request.setAttribute("nombre", nombre);*/
-			
-			return mapping.findForward("Bienvenido");
-		}
-	 
+
+//import com.mkyong.common.form.HelloWorldForm;
+
+public class Controlador extends Action {
+
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		/*
+		 * BienvenidaForm bienvenida = (BienvenidaForm) form;
+		 * bienvenida.setMessage("Bienvenido a Struts");
+		 */
+
+		/*
+		 * String texto = (String) request.getParameter("Texto"); String nombre
+		 * = (String) request.getParameter("Nombre");
+		 * request.setAttribute("texto", texto); request.setAttribute("nombre",
+		 * nombre);
+		 */
+
+		return mapping.findForward("Bienvenido");
 	}
 
+}
